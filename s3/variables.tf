db51132c37b3470d31e27c61378ad48ae9679c00
Vengalrao-eppa <rao.evs60@gmail.com>
2025-03-28 14:18:47 +0000

    adding s3 bucket with permissions

---
+++ s3/variables.tf
@@ -0,0 +1,10 @@
+variable "oidc_connect_provider_arn" {
+  description = "ARN of the OIDC provider"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block of the VPC"
+  type        = string
+}
+
